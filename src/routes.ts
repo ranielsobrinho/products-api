e3c11e410eab2bfb1d1fbd13c6a16b3c0b8bb772
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-27 18:32:38 -0300

    feat: add new route to create a product

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import GetProductsController from "./controller/ProductsController/GetProductsController";
+import CreateProductController from "./controller/ProductsController/CreateProductController";
 
 const routes = Router()
 
 routes.get('/products', GetProductsController.execute)
+routes.post('/products', CreateProductController.execute)
 
 export default routes
