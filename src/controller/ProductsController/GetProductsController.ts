73c8a4819e7a91b82608a9120e65d28769b07175
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-27 16:58:29 -0300

    feat: add controller to return all products from service

---
+++ src/controller/ProductsController/GetProductsController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { IResponse, ResponseStatus } from "../../utils/service";
+import GetAllProductsService from "../../service/ProductService/GetAllProductsService";
+
+class GetProductsController {
+  async execute(req: Request, res: Response<IResponse>): Promise<Response<IResponse>> {
+    try {
+      const products = await GetAllProductsService.handle()
+
+      return res.json({
+        status: ResponseStatus.OK,
+        data: products
+      })
+    } catch (error) {
+      return res.status(500).json({
+        status: ResponseStatus.INTERNAL_SERVER_ERROR,
+        message: 'Sorry, something wrong just happened.'
+      })
+    }
+  }
+}
+
+export default new GetProductsController()
