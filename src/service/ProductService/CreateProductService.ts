884e32a39923d22fb0bbdd788ffccddc2de8ea56
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-27 18:31:24 -0300

    feat: add service to insert products on database

---
+++ src/service/ProductService/CreateProductService.ts
@@ -0,0 +1,28 @@
+import { getRepository } from "typeorm";
+import { produtos } from "../../domain/entity/produtos";
+
+interface productsRequest {
+  nome_produto: string
+  fabricante: string
+  quantidade_estoque: number
+  valor: number
+}
+
+class CreateProductService {
+  async handle({ nome_produto, fabricante, quantidade_estoque, valor }: productsRequest): Promise<produtos | Error> {
+    const repo = getRepository(produtos)
+    if (await repo.findOne({ nome_produto })) {
+      return new Error('Product already created. Choose another product name.')
+    }
+    const product = repo.create({
+      nome_produto,
+      fabricante,
+      quantidade_estoque,
+      valor
+    })
+    await repo.save(product)
+    return product
+  }
+}
+
+export default new CreateProductService()
