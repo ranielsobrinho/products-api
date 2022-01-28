dc2549a92a575cf3993ff7510ef7bb2e40fe53aa
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-28 11:48:33 -0300

    feat: add route to delete a product

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { GetProductsController, CreateProductController, UpdateProductController } from "./controller/ProductsController/";
+import { GetProductsController, CreateProductController, UpdateProductController, DeleteProductController } from "./controller/ProductsController/";
 
 const routes = Router()
 
 routes.get('/products', GetProductsController.execute)
 routes.post('/products', CreateProductController.execute)
 routes.put('/products/:id', UpdateProductController.execute)
+routes.delete('/products/:id', DeleteProductController.execute)
 
 export default routes
