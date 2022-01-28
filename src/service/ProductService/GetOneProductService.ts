ef24c98d77be21e7f3ceffaf6d1cbfd2b423d300
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-28 15:35:30 -0300

    feat: add new service to get one product by id

---
+++ src/service/ProductService/GetOneProductService.ts
@@ -0,0 +1,17 @@
+import { getRepository } from "typeorm";
+import { produtos } from "../../domain/entity/produtos";
+
+class GetOneProductService {
+  async handle(id: string): Promise<produtos | Error> {
+    const repo = getRepository(produtos)
+    const product = await repo.findOne(id, {
+      select: ['id', 'nome_produto', 'fabricante', 'quantidade_estoque', 'valor']
+    })
+    if (!product) {
+      return new Error('No product exists with this id.')
+    }
+    return product
+  }
+}
+
+export default new GetOneProductService()
