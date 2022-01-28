092e0f0ecd3f6f45b43a73d93b48a7340da221d2
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-28 15:37:13 -0300

    feat: add new route to get a product by id

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { GetProductsController, CreateProductController, UpdateProductController, DeleteProductController } from "./controller/ProductsController/";
+import { GetProductsController, CreateProductController, UpdateProductController, DeleteProductController, GetOneProductController } from "./controller/ProductsController/";
 
 const routes = Router()
 
 routes.get('/products', GetProductsController.execute)
+routes.get('/products/:id', GetOneProductController.execute)
 routes.post('/products', CreateProductController.execute)
 routes.put('/products/:id', UpdateProductController.execute)
 routes.delete('/products/:id', DeleteProductController.execute)
