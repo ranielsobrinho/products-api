4a0fe17975febd40ec8b61e48b84c5b1b9b36fa2
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-28 11:32:13 -0300

    chore: add export main file to import all controllers into one line on routes file

---
+++ src/controller/ProductsController/index.ts
@@ -0,0 +1,9 @@
+import CreateProductController from "./CreateProductController";
+import GetProductsController from "./GetProductsController";
+import UpdateProductController from "./UpdateProductController";
+
+export {
+  CreateProductController,
+  GetProductsController,
+  UpdateProductController
+}
