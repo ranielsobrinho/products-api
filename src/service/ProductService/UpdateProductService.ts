c65547d951fa9a11647a7fe821d1976a7daaf5fc
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-28 11:32:41 -0300

    feat: add new service to update product data

---
+++ src/service/ProductService/UpdateProductService.ts
@@ -0,0 +1,29 @@
+import { getRepository } from "typeorm";
+import { produtos } from "../../domain/entity/produtos";
+
+interface updateRequest {
+  id: string
+  nome_produto: string
+  fabricante: string
+  quantidade_estoque: number
+  valor: number
+}
+
+class UpdateProductService {
+  async handle({ id, nome_produto, fabricante, quantidade_estoque, valor }: updateRequest) {
+    const repo = getRepository(produtos)
+    if (! await repo.findOne(id)) {
+      return new Error('No existing product with this id.')
+    }
+    const update = await repo.update(id, {
+      nome_produto,
+      fabricante,
+      quantidade_estoque,
+      valor
+    })
+
+    return update
+  }
+}
+
+export default new UpdateProductService()
