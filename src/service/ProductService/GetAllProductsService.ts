a6f2b7dc7b7f8a4f00519d8621bb4bcc06d4fd72
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-28 15:34:54 -0300

    refactor: select fields of product table

--- src/service/ProductService/GetAllProductsService.ts
+++ src/service/ProductService/GetAllProductsService.ts
@@ -4,7 +4,9 @@ import { produtos } from "../../domain/entity/produtos";
 class GetAllProductsService {
   async handle() {
     const repo = getRepository(produtos)
-    const products = await repo.find()
+    const products = await repo.find({
+      select: ['id', 'nome_produto', 'fabricante', 'quantidade_estoque', 'valor']
+    })
 
     return products
   }
