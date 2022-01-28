541163eb3ab9aeb21c1568cc3a374c5de9593d92
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-28 20:13:32 -0300

    feat: use middleware on routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { GetProductsController, CreateProductController, UpdateProductController, DeleteProductController, GetOneProductController } from "./controller/ProductsController/";
+import validate from "./middlewares/ValidationMiddleware";
+import productSchema from "./utils/validations/productValidation";
 
 const routes = Router()
 
 routes.get('/products', GetProductsController.execute)
 routes.get('/products/:id', GetOneProductController.execute)
-routes.post('/products', CreateProductController.execute)
-routes.put('/products/:id', UpdateProductController.execute)
+routes.post('/products', validate(productSchema), CreateProductController.execute)
+routes.put('/products/:id', validate(productSchema), UpdateProductController.execute)
 routes.delete('/products/:id', DeleteProductController.execute)
 
 export default routes
