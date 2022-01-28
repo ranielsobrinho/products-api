3031ba0195bd125104561d70806e7bc3517f9dd1
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-28 11:32:55 -0300

    feat: add new controller to update product data

---
+++ src/controller/ProductsController/UpdateProductController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { IResponse, ResponseStatus } from "../../utils/service";
+import UpdateProductService from "../../service/ProductService/UpdateProductService";
+
+class UpdateProductController {
+  async execute(req: Request, res: Response<IResponse>): Promise<Response<IResponse>> {
+    try {
+      const { id } = req.params
+      const { nome_produto, fabricante, quantidade_estoque, valor } = req.body
+      const updated = await UpdateProductService.handle({ id, nome_produto, fabricante, quantidade_estoque, valor })
+
+      if (updated instanceof Error) {
+        return res.status(400).json({
+          status: ResponseStatus.BAD_REQUEST,
+          message: updated.message
+        })
+      }
+
+      return res.json({
+        status: ResponseStatus.OK,
+        data: updated,
+        message: 'Updated successfully.'
+      })
+    } catch (error) {
+      return res.status(500).json({
+        status: ResponseStatus.INTERNAL_SERVER_ERROR,
+        message: 'Sorry, something wrong just happened.'
+      })
+    }
+  }
+}
+
+export default new UpdateProductController()
