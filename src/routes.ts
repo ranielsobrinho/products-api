b0fc90c036661407293655b35968068cf4a77c73
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-31 16:12:45 -0300

    feat: add new route to create a new manufacturer and add yup validator middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { GetProductsController, CreateProductController, UpdateProductController, DeleteProductController, GetOneProductController } from "./controller/ProductsController/";
+import CreateManufactureController from "./controller/ManufactureController/CreateManufactureController";
 import validate from "./middlewares/ValidationMiddleware";
-import productSchema from "./utils/validations/productValidation";
+import { manufactureSchema, productSchema } from "./utils/validations/";
 
 const routes = Router()
 
@@ -10,5 +11,7 @@ routes.get('/products/:id', GetOneProductController.execute)
 routes.post('/products', validate(productSchema), CreateProductController.execute)
 routes.put('/products/:id', validate(productSchema), UpdateProductController.execute)
 routes.delete('/products/:id', DeleteProductController.execute)
+
+routes.post('/manufacture', validate(manufactureSchema), CreateManufactureController.execute)
 
 export default routes
