52df88228f331d2949acab5ebe4df3b541cb6a43
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-01-31 18:12:07 -0300

    refactor: change min and max of telefone field

--- src/utils/validations/manufactureValidation.ts
+++ src/utils/validations/manufactureValidation.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup'
 
 const manufactureSchema = yup.object({
   nome_fabricante: yup.string().required("Manufacturer name is required."),
-  telefone: yup.number().min(9).max(9).required("Phone number is required.")
+  telefone: yup.number().required("Phone number is required.")
 })
 
 export default manufactureSchema
